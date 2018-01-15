09253d2c7b5f715b567e7e5608b73d5422c968d0
Ronny Worm <r-worm@gmx.de>
2018-01-15 14:45:23 +0100

    View db2 wavelet of samples

--- plot_mfcc.py
+++ plot_mfcc.py
@@ -6,45 +6,34 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import pywt
 
-nprows = None
-meta = None
-with open('outmfcc_D_A.csv', 'r') as csvfile:
+
+samples = None
+with open('samples.csv', 'r') as csvfile:
     thereader = csv.reader(csvfile, delimiter=';', quotechar='\'')
     rows = []
-    first = True
+    index = 0
     for row in thereader:
-    	if first: # skip columnnames
-    		meta = row
-    		first = False
-    	else:
-        	rows += [row]
+        if index < 2: # skip columnnames
+            index += 1
+        else:
+            rows += [row]
 
-    nprows = np.array(rows).T
+    samples = np.array(rows).T
     
-timestep = nprows[1].astype("float64")
-values = nprows[2:].astype("float64")
+timesteps = samples[0].astype("float64")
+values = samples[1].astype("float64")
 
-def f(x, y):
-    return values[y,x]
+cA, cD = pywt.dwt(samples[1], "db2")
 
-x = np.linspace(0, 178, 179).astype("int64")
-y = np.linspace(0, 38, 39).astype("int64")
+# siehe Verstärkung am Ende der Zeile
+plt.plot(timesteps, values * 50)
+plt.plot(timesteps[:len(cA)], cA * 50)
+plt.plot(timesteps[:len(cD)], cD * 50)
 
-X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
+plt.title("Samples with db2 Wavelet")
+plt.legend(['samples', 'approximation coefficients', 'detail coefficients'], loc='upper left')
+plt.xticks(np.arange(min(timesteps), max(timesteps)+0.2, 0.2))
 
-fig = plt.figure()
-from mpl_toolkits import mplot3d
-ax = plt.axes(projection='3d')
-ax.view_init(20, 30)
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                cmap='viridis', edgecolor='none')
-ax.set_xlabel('timestep')
-ax.set_ylabel('mfcc no')
-ax.set_zlabel('value');
 plt.show()
-
-
-#plt.xticks(np.arange(0, nprows.shape[1] - 2, 2))
-#plt.yticks(np.arange(-60, 180, 20))
